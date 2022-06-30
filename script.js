98fd81ee2296ea781c12c604e4041cbf0023517b
ijeomaese <ijeomaodinko@gmail.com>
2022-06-30 04:38:27 -0400

    Add codes to complete the website

--- script.js
+++ script.js
@@ -21,50 +21,109 @@ class BookCollection {
 }
 
 const books = document.querySelector(".book-collection");
-const add = document.querySelector(".button");
+const addButton = document.querySelector(".add");
 const inputtitle = document.querySelector(".input-title");
 const inputauthor = document.querySelector(".input-author");
-
 const collection = new BookCollection();
 
+// to set collection to same if there is collection stored
 if (localStorage.getItem("data") !== null) {
   collection.booklist = JSON.parse(localStorage.getItem("data"));
 }
 
-function remove(event) {
+// to remove book from collection
+function removeElement(event) {
   collection.removeBook(event.target.className);
-  refreshPage();
+
+  // to remove book from the collection
+  const containertoremove = document.querySelector(
+    `.${event.target.className}`
+  );
+  containertoremove.remove();
+
+  // to store new collection in Local Storage
+  localStorage.setItem("data", JSON.stringify(collection.booklist));
 }
 
-function refreshPage() {
-  books.innerHTML = "";
-  for (let i = 0; i < collection.booklist.length; i += 1) {
-    const container = document.createElement("div");
-    container.className = "container";
-    books.appendChild(container);
+// to create container to place added books and remove button
+function createBookElement(bookname, bookauthor) {
+  const container = document.createElement("div");
+  container.className = `container ${bookname}`;
+  books.appendChild(container);
 
-    const titledisplay = document.createElement("p");
-    titledisplay.innerHTML = `"${collection.booklist[i].title}" by ${collection.booklist[i].author}`;
-    container.appendChild(titledisplay);
+  // to create book title element
+  const titledisplay = document.createElement("p");
+  titledisplay.innerHTML = `"${bookname}" by ${bookauthor}`;
+  container.appendChild(titledisplay);
 
-    const buttonremove = document.createElement("button");
-    buttonremove.innerHTML = "Remove";
-    buttonremove.classList = collection.booklist[i].title;
-    buttonremove.addEventListener("click", remove);
-    container.appendChild(buttonremove);
-  }
-
-  inputtitle.value = "";
-  inputauthor.value = "";
-  localStorage.setItem("data", JSON.stringify(collection.booklist));
+  // to create remove button element
+  const buttonremove = document.createElement("button");
+  buttonremove.innerHTML = "Remove";
+  buttonremove.classList = bookname;
+  buttonremove.addEventListener("click", removeElement);
+  container.appendChild(buttonremove);
 }
 
 function addtocollection() {
   const booktoadd = new Book(inputtitle.value, inputauthor.value);
   collection.addBook(booktoadd);
-  refreshPage();
+
+  createBookElement(booktoadd.title, booktoadd.author);
+
+  // to reset form input values
+  inputtitle.value = "";
+  inputauthor.value = "";
+
+  // to store new collection in Local Storage
+  localStorage.setItem("data", JSON.stringify(collection.booklist));
 }
 
-add.addEventListener("click", addtocollection);
+function updatePage() {
+  for (let i = 0; i < collection.booklist.length; i += 1) {
+    createBookElement(
+      collection.booklist[i].title,
+      collection.booklist[i].author
+    );
+  }
+}
 
-refreshPage();
+addButton.addEventListener("click", addtocollection);
+updatePage();
+
+//to attach the class to a variable names that will help in diplaying of website
+const list = document.querySelector(".list");
+const addNew = document.querySelector(".addnew");
+const contact = document.querySelector(".contact");
+
+const list_Section = document.querySelector(".booklist");
+const form_Section = document.querySelector(".form");
+const contact_Section = document.querySelector(".contact-information");
+
+//to declare the functions that will used in displaying websites
+function displaylist() {
+  list_Section.classList.remove("hidden");
+  form_Section.classList.add("hidden");
+  contact_Section.classList.add("hidden");
+}
+
+function displayaddnew() {
+  list_Section.classList.add("hidden");
+  form_Section.classList.remove("hidden");
+  contact_Section.classList.add("hidden");
+}
+
+function displaycontact() {
+  list_Section.classList.add("hidden");
+  form_Section.classList.add("hidden");
+  contact_Section.classList.remove("hidden");
+}
+
+//to display each of the websites
+list.onclick = displaylist;
+addNew.onclick = displayaddnew;
+contact.onclick = displaycontact;
+
+//to display the current date on the website
+const date = document.querySelector(".date");
+const datetodisplay = new Date();
+date.innerHTML = datetodisplay;
